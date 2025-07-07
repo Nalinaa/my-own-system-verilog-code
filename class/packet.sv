8bd6b9e37067cd18575b8d1c1da7f7bb866546d7
Nalinaa <114544530+Nalinaa@users.noreply.github.com>
2025-07-07 07:09:13 +0530

    Create packet.sv

---
+++ class/packet.sv
@@ -0,0 +1,27 @@
+/*Create a class Packet with the following properties:
+
+bit [7:0] addr
+
+bit [7:0] data
+
+Write a method to display the packet details.
+
+Create 2 objects and assign different values.*/
+
+class packet;
+  bit [7:0] addr;
+  bit [7:0] data;
+  function void display();
+    $display("The add = %d and data = %d",addr,data);
+  endfunction
+endclass
+
+module top;
+  packet pkt = new();
+  initial
+    begin
+      pkt.addr = 6;
+      pkt.data = 25;
+      pkt.display();
+    end
+endmodule
